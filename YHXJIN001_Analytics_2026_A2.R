ae5b91283cc00032a48d0de17c8ddad274900828
Jing Yeh <jing@mohara.co>
2026-04-19 18:39:17 +0200

    chore: remove unused code

--- YHXJIN001_Analytics_2026_A2.R
+++ YHXJIN001_Analytics_2026_A2.R
@@ -14,9 +14,9 @@ hist(data$Fluid)
 hist(data$AveSpeed)
 boxplot(data)
 # From the summary and the boxplot, we see that the features have ranges that vary greatly.
-# This is bad for neural networks. We must thus standardise the data to make sure no variables
-# Dominate
-data_scaled = as.data.frame(scale(data))
+# This is bad for neural networks. We would have needed to rescale the data if we were using 
+# the standard gradient descent. However, R's nlm uses a Newton like second order derivate optimisation
+# algorithm, which implicitly takes care of the scaling
 summary(data_scaled)
 par(mfrow = c(1, 1))
 
@@ -159,6 +159,8 @@ best_index = which.min(errors)
 best_nu = nu_grid[best_index]
 theta_best_nu = theta_store[best_index,]
 
+
+
 # Response Curve over Carbs, Sex, Fluid
 
 library(colorspace)
@@ -174,9 +176,6 @@ carbs_seq = seq(min(data$Carbs),max(data$Carbs),length.out= M)
 carbs_mean = mean(data$Carbs)
 fluid_seq = seq(min(data$Fluid),max(data$Fluid),length.out= M)
 fluid_mean = mean(data$Fluid)
-
-
-
 
 xxv1 = rep(carbs_seq, M)
 xxv2 = rep(fluid_seq, each =M)
@@ -201,84 +200,5 @@ plot(xxv2 ~ xxv1, col=color.gradient(response_male$Y_hat), pch=16, cex=0.8,
 legend("topright", title="Risk of Bombing",
        legend=c("High (Near 100%)", "Medium (~50%)", "Low (Near 0%)"), 
        col=c("skyblue1", "purple", "coral"), pch=16, bg="white", cex=0.5)
-
-
-Xu_male = data.frame(AveSpeed=12, Sex=0)
-Xu_male = Xu_male[rep(seq_len(nrow(Xu_male)), each = 100), ]
-Xu_female = data.frame(AveSpeed=12, Sex=1)
-Xu_female = Xu_female[rep(seq_len(nrow(Xu_female)), each = 100), ]
-Xv_carbs = data.frame(Carbs=carbs_seq, fluid=fluid_mean)
-Xv_fluid = data.frame(Carbs=carbs_mean, fluid=fluid_seq)
-X_carbs = data.frame(AveSpeed=12, Sex=0, Fluid=1)
-
-pred_carbsMale = neural_net(Xu_male, Xv_carbs, 4, theta_best_nu, best_nu)
-pred_carbsFemale = neural_net(Xu_female, Xv_carbs, 4, theta_best_nu, best_nu)
-pred_fluidMale = neural_net(Xu_male, Xv_fluid, 4, theta_best_nu, best_nu)
-pred_fluidFemale =neural_net(Xu_female, Xv_fluid, 4, theta_best_nu, best_nu)
-
-
-plot(as.vector(pred_carbsMale$Y_hat) ~ carbs_seq, type="l", col="coral1", lwd=2, 
-     xlab="Carbs", ylab="Probability of Bombing", main="Response to Carbs by Sex", ylim=c(0,1))
-lines(as.vector(pred_carbsFemale$Y_hat) ~ carbs_seq, type="l", col="skyblue4", lwd=2)
-legend("bottomleft", legend=c("Male (Sex=0)", "Female (Sex=1)"), 
-       col=c("coral1", "skyblue4"), lty=1, lwd=2, cex=0.5)
-
-plot(as.vector(pred_fluidMale$Y_hat) ~ fluid_seq, type="l", col="hotpink", lwd=2, 
-     xlab="Fluid", ylab="Probability of Bombing", main="Response to Fluid by Sex")
-lines(as.vector(pred_fluidFemale$Y_hat) ~ fluid_seq, type="l", col="black", lwd=2)
-legend("bottomleft", legend=c("Male (Sex=0)", "Female (Sex=1)"), 
-       col=c("hotpink", "black"), lty=1, lwd=2, cex=0.5)
-
-
-# Interpretation: It seems like males, in general, are able to consume less carbs, and drink more fluid to avoid getting bombed.
-# On the other hand, female atheletes cannot drink as much fluid as males, and require more carbs to not getting bombed
-
-# We recommend male runners to aim to consume between 1l to 1.8l of fluid,, 50g to 90g of carbs when running at 12km/hr
-# Female runners should aim to consume between 1l to 1.6l of fluid, 60g to 90g of carbs when running at 12km/hr.
-
-# Empirical advantage of using a split brain network
-
-# Just in case we need cross validation
-set.seed(2026)
-cross_validation = function(Xu, Xv, folds, nu){
-  total_valid_error = 0
-  N = dim(X)[1]
-  fold_size = floor(N/5)
-  for (i in 1:folds){
-    if (i != folds){
-      valid_indices = (i-1) * fold_size : i * fold_size
-    }
-    else{
-      # Handle last trailing values
-      valid_indices= (i-1) * fold_size:-1
-    }
-    Xu_train = Xu[-valid_indices, ]
-    Xu_valid = Xu[valid_indices,]
-    Xv_train = Xv[-valid_indices, ]
-    Xv_valid = Xv[valid_indices,]
-    Y_train = as.matrix(Y[-valid_indices])
-    Y_valid = as.matrix(Y[valid_indices])
-    
-    nparams = (pu * m) + (pv * m) + 2 * m  + 2 * m * 2 + 2 + 2 + 1
-    theta = runif(nparams, -1, 1)
-    
-    # Wrapper function to train NN with nlm
-    obj_train = function(theta_init) {
-      result = neural_net(Xu_train, Xv_train, 4, theta_init, nu)
-      return(result$pred)
-    }
-    result_fold = nlm(neural_net, theta)
-    total_valid_error = neural_net(Xu_valid, Xv_valid, 4, result_fold$esimate, Y_valid, nu)
-    
-    return (total_valid_error/folds)
-  }
-  
-  
-}
-
-# 80-20 splits mean we do 5 folds cross validation
-result = nlm(obj_train, theta)
-
-
 
 
